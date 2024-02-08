b0a5dd5937b64b6832b4b70877273cffa6c60de4
Raheja-Sarthak <109036743+Raheja-Sarthak@users.noreply.github.com>
2024-02-09 04:44:10 +0530

    tcount-pulse added

--- counter.v
+++ counter.v
@@ -1,42 +1,54 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 03.02.2024 17:11:22
-// Design Name: 
+// Design Name:
 // Module Name: counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module counter(
-    input clk, 
+    input clk,
     input rst,
     input en,
     input [7:0] load,
-    output reg [7:0] count
-    );
-    
-    always @(posedge clk)
+    output reg [7:0] count,
+    output reg tcount
+  );
+
+  always @(posedge clk)
+  begin
+
+    if(rst)
     begin
-    
-        if(rst)
-            count <= 0;
-        else if (en)
-            count <= load;
-        else
-            count <= count + 1;      
-    
+      count <= 0;
+      tcount <= 0;
     end
+    else if (en)
+      count <= load;
+    else if (count == 255)
+    begin
+      tcount <= 1;
+      count <= count + 1;
+    end
+    else
+    begin
+      count <= count + 1;
+      tcount <= 0;
+    end
+
+  end
 endmodule
